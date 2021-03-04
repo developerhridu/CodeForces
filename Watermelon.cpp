46afce4e3493a0639a5f66584158c4411400844d
developerhridu <72350795+developerhridu@users.noreply.github.com>
2021-03-04 17:50:50 +0600

    Initial commit

---
+++ Watermelon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n <= 2 || n % 2 != 0)
+    {
+        cout << "NO\n";
+    }
+    else
+    {
+        cout << "YES\n";
+    }
+    return 0;
+}
